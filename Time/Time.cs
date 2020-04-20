a6766191e24571eea6098cfc2dff4952fd36a17f
JarnoTainio <47140523+JarnoTainio@users.noreply.github.com>
2020-04-20 08:41:26 +0300

    Add files via upload

---
+++ Time/Time.cs
@@ -0,0 +1,104 @@
+﻿public struct Time
+{
+    public static Time Zero = new Time(0, 0, 0, 0, 0);
+
+    public static int MONTHS_IN_YEAR = 12;
+    public static int DAYS_IN_MONTH = 28;
+    public static int HOURS_IN_DAY = 24;
+
+    public static int Hour = 60;
+    public static int Day = Hour * HOURS_IN_DAY;
+    public static int Month = Day * DAYS_IN_MONTH;
+    public static int Year = Month * MONTHS_IN_YEAR;
+
+    public int year;
+    public int month;
+    public int day;
+    public int hour;
+    public int minutes;
+
+    public int time;
+
+    public Time(int y, int m, int d, int h, int min)
+    {
+        year = y;
+        month = m;
+        day = d;
+        hour = h;
+        minutes = min;
+        time = year * MONTHS_IN_YEAR * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += month * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += day * HOURS_IN_DAY * 60;
+        time += hour * 60;
+        time += minutes;
+    }
+
+    public Time(string str)
+    {
+        string[] parts = str.Split();
+        year = int.Parse(parts[0]);
+        month = int.Parse(parts[1]);
+        day = int.Parse(parts[2]);
+        hour = int.Parse(parts[3]);
+        minutes = int.Parse(parts[4]);
+        time = year * MONTHS_IN_YEAR * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += month * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += day * HOURS_IN_DAY * 60;
+        time += hour * 60;
+        time += minutes;
+    }
+
+    public int Difference(Time t)
+    {
+        return t.time - time;
+    }
+
+    private void UpdateTime()
+    {
+        time = year * MONTHS_IN_YEAR * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += month * DAYS_IN_MONTH * HOURS_IN_DAY * 60;
+        time += day * HOURS_IN_DAY * 60;
+        time += hour * 60;
+        time += minutes;
+    }
+
+    public int DaysPassed()
+    {
+        return DaysPassed(TimeManager.Now());
+    }
+
+    public int DaysPassed(Time time)
+    {
+        return time.ToDayInt() - ToDayInt();
+    }
+
+    public int MinutesPassed()
+    {
+        return MinutesPassed(TimeManager.Now());
+    }
+
+    public int MinutesPassed(Time time)
+    {
+        return time.time - this.time;
+    }
+
+    public int ToDayInt()
+    {
+        return ((year * MONTHS_IN_YEAR + month) * DAYS_IN_MONTH + day);
+    }
+
+    public override string ToString()
+    {
+        return "Year: " + year + " Month: " + month + " Day: " + day + " Hour: " + hour;
+    }
+
+    public string ToShortString()
+    {
+        return year + "." + month + "." + day + "." + hour + ":" + minutes;
+    }
+
+    public string ToDateString()
+    {
+        return "Year: " + year + " Month: " + month + " Day: " + day;
+    }
+}
---
+++ Time/TimeManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class TimeManager
+{
+    public static int minutes;
+    public static int hour;
+    public static int day;
+    public static int month;
+    public static int year;
+
+    public static void Reset()
+    {
+        year = 0;
+        month = 1;
+        day = 1;
+        hour = 7;
+        minutes = 0;
+    }
+
+    public static bool Tick(int delta)
+    {
+        if (delta > 60)
+        {
+            Dummy.PrintMessage("TIME ERROR!");
+        }
+        minutes += delta;
+        while (minutes >= 60)
+        {
+            minutes -= 60;
+
+            hour++;
+            while (hour > 24)
+            {
+                hour = 1;
+                day++;
+
+                while (day > 28)
+                {
+                    day = 1;
+                    month++;
+                    while (month > 12)
+                    {
+                        month = 1;
+                        year++;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Time Now(bool onlyDay = false)
+    {
+        return new Time(year, month, day, onlyDay ? 0 : hour, onlyDay ? 0 : minutes);
+    }
+
+    public static string GetCompactTime()
+    {
+        string str = "";
+        str += "[";
+        str += (year < 10 ? "000" : (year < 100 ? "00" : (year < 1000 ? "0" : ""))) + year + ":";
+        str += (month < 10 ? "0":"") + month + ":";
+        str += (day < 10 ? "0" : "") + day + ":";
+        str += (hour < 10 ? "0" : "") + hour + ":";
+        str += (minutes < 10 ? "0" : "") + minutes;
+        str += "]";
+        return str;
+    }
+}
